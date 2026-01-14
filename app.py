58d7d839638fc0386dc37fe5adf0fa1edbd7f73a
AAron <akempley@gmail.com>
2026-01-13 20:53:39 -0800

    fixed Flask routes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,19 +7,10 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
-=======
 
-app = Flask(__name__)
 
 # App metadata
 APP_NAME = "Movie Archiver"
 APP_VERSION = "1.0.0"
 APP_DESCRIPTION = "A web application that archives and reccomends movies"
 
-@app.route("/")
-def home():
-    return "Hello Team Alpha!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> main
