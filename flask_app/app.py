32448019bbe97eadf1343edbf6a12dc58793c889
silky <poppasilky@gmail.com>
2026-01-28 01:51:08 -0800

    fix: update flask_app/app.py with clean version

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,26 +1,15 @@
 from flask import Flask
-<<<<<<< HEAD
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == '__main__':
-    app.run()
-=======
 
 app = Flask(__name__)
 
 # App metadata
 APP_NAME = "Movie Archiver"
 APP_VERSION = "1.0.0"
-APP_DESCRIPTION = "A web application that archives and reccomends movies"
+APP_DESCRIPTION = "A web application that archives and recommends movies"
 
 @app.route("/")
 def home():
     return "Hello Team Alpha!"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> main
